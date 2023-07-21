e47b6fa7a3a5bc7f5c3c8e658049a82705c5fc93
Prince <hawazprince@gmail.com>
2023-07-21 11:18:42 -0400

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c35c7ed35fmshcb78ba37ddf9e22p11d8a3jsn4484c92afb16',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "11b67a4a36ec81d2778eb1c76558f217";
